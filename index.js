63c104a692b34c69ffa07a05bd24efeec4e2e174
fatihbaltaci <baltacifatih14@gmail.com>
2022-12-26 17:12:34 +0300

    Update

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const core = require('@actions/core');
+const axios = require('axios');
+
 
 // Run on every change: npm run prepare
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    var body = JSON.stringify({
+    var body = {
       "target": core.getInput('target'),
       "locations": core.getInput('locations')
-    });
-
-    var requestOptions = {
-      method: 'POST',
-      headers: {
-        "X-API-KEY": core.getInput('api_key'),
-        "Content-Type": "application/json"
-      },
-      body: body,
-      redirect: 'follow'
     };
 
-    fetch("https://api.ddosify.com/v1/latency/test/", requestOptions)
-      .then(response => response.text())
-      .then(result => core.setOutput('result', JSON.stringify(result)))
+    const response = await axios.post(
+      'https://api.ddosify.com/v1/latency/test/',
+      body,
+      {
+        headers: {
+          "X-API-KEY": core.getInput('api_key'),
+          "Content-Type": "application/json",
+          "Accept-Encoding": "gzip,deflate,compress"
+        }
+      }
+    ).then(function (response) {
+      console.log(response.data);
+      core.setOutput('result', JSON.stringify(result));
+    });
 
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message, error.body);
   }
 }
 
