196cd916fb2c7656438d74f17be03e7e7711500e
fatihbaltaci <baltacifatih14@gmail.com>
2022-12-28 15:57:24 +0300

    Update

--- index.js
+++ index.js
@@ -31,22 +31,22 @@ function getConcatenatedLocations(obj) {
 // Run on every change: npm run prepare
 async function run() {
 
-  const failIfText = core.getInput('failIf').trim(); // any>100 or NA.US.MA.BO>80
-  const target = core.getInput('target')  // https://example.com
-  const locationsInput = JSON.parse(core.getInput('locations'))  // ["*"]
-  const apiKey = core.getInput('api_key')
+  const failIfText = "any>100"; // any>100 or NA.US.MA.BO>80
+  const target =   "https://app.servdown.com"
+  const locationsInput = ["*"]
+  const apiKey = "8e745f05-e1bd-41bf-9276-ac7689e640e6"
 
   const failIfArr = failIfText.split(">")
   const failIfLocation = failIfArr[0].trim()
-  const failIfLatency = failIfArr[1].trim()
+  const failIfLatency = parseInt(failIfArr[1].trim())
   let failedLocations = []
   response = {}
   if (failIfText !== "") {
-    core.info(`\u001b[33mFail if:   ${failIfText}`)
+    core.info(`\u001b[33mFail if:   ${failIfText} \x1b[0m`)
   }
 
-  core.info(`\u001b[33mTarget:    ${target}`)
-  core.info(`\u001b[33mLocations: ${locationsInput}`)
+  core.info(`\u001b[33mTarget:    ${target} \x1b[0m`)
+  core.info(`\u001b[33mLocations: ${locationsInput} \x1b[0m`)
 
   try {
     const responseLocations = await axios.get(
@@ -77,15 +77,37 @@ async function run() {
 
     const locations = getConcatenatedLocations(responseLocations.data);
 
+    console.log(`┌────────────────────────────────────────────────────────────────────────────────────┐`);
+    console.log(`| ${"Location".padEnd(20)} | ${"Location Code".padEnd(15)} | ${"Status Code".padEnd(8)} | ${"Latency (ms)".padEnd(10)} | ${"Fail If (ms)".padEnd(8)} |`);
+    console.log(`├────────────────────────────────────────────────────────────────────────────────────┤`);
     for (let locKey in responseTest.data) {
+      logText = ""
       response[locKey + 1] = { ...{ "location": locations[locKey] }, ...{ "location_code": locKey }, ...responseTest.data[locKey] }
       latency = responseTest.data[locKey]["latency"];
-      if (failIfText !== "" && ((failIfLocation === "any" && latency > failIfLatency) || (locKey === failIfLocation && latency > failIfLatency))) {
-        failedLocations.push(`${latency},${locations[locKey]},${failIfLatency}`)
+      statusCode = responseTest.data[locKey]["status_code"]
+      if (statusCode === undefined){
+        statusCode = "Fail"
       }
+      latency = responseTest.data[locKey]["latency"]
+      if (latency === undefined){
+        latency = "Fail"
+      }
+      logText = `| ${locations[locKey].padEnd(20)} | ${locKey.padEnd(15)} |    ${statusCode.toString().padEnd(8)} |     ${latency.toString().padEnd(8)} |`;
+
+      if (failIfText !== "" && ((failIfLocation === "any") || (locKey === failIfLocation))) {
+        logText += `    >${failIfLatency.toString().padEnd(8)} |`
+        if (latency !== "Fail" && latency > failIfLatency){
+          failedLocations.push(`${latency},${locations[locKey]},${failIfLatency}`)
+          logText = "\u001b[91m" + logText + "\x1b[0m"
+        }
+      }
+      else {
+        logText += `${"".padEnd(13)} |`
+      }
+      core.info(logText);
     }
     core.setOutput("result", response);
-    console.table(response);
+    // console.table(response);
 
     for (let i in failedLocations) {
       const fLoc = failedLocations[i].split(",")
